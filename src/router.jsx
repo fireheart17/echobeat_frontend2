e54adc495e760060af861c418b1712806a5b120b
ShivanshK <shivansh.kandpal.cse22@itbhu.ac.in>
2024-11-04 04:16:14 +0530

    fix

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Fyp from "./pages/Fyp";
+import Playlist from "./pages/Playlist";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Fyp />,
   },
+  {
+    path: "/playlist/:id",
+    element: <Playlist />,
+  },
+  {
+    path: "/charts",
+    element: <Charts />,
+  },
+  {
+    path: "/chart/:id",
+    element: <Chart />,
+  }
 ]);
